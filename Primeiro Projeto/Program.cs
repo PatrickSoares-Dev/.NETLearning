665891fd0a20ce1bc8d18174b6a959882aaefedf
Patrick Oliveira <patrickoliveiramoto@gmail.com>
2022-08-03 17:57:27 -0300

    Primeiro Commit

---
+++ Primeiro Projeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primeiro_Projeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
