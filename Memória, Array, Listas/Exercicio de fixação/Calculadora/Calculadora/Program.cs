66fc5cf55b674355e5e0f97f4f5f791d9a763917
Patrick Oliveira <patrick.oliveira@telecall.com>
2022-08-31 14:55:49 -0300

    Exercicios com vetores, for e foreach

---
+++ Memória, Array, Listas/Exercicio de fixação/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Numbers = Calculadora.Sum();
+
+            string[] vect = new string[] { "Maria", "Bob", "Marcos" };
+
+            // Foreach é uma maneira simplificada do FOR
+
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
